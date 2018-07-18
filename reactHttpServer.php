0530d96abda70431b63c9d8fb4ff72bf6b9ca7d0
Félix Carpena <felix@carpena.eu>
2018-07-18 22:43:59 +0200

    chore: first commit - basic reactphp http server

---
+++ reactHttpServer.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+
+$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
+    return new React\Http\Response(
+        200,
+        array('Content-Type' => 'text/plain'),
+        "Hello World!\n"
+    );
+});
+
+$socket = new React\Socket\Server('0.0.0.0:8081', $loop);
+$server->listen($socket);
+
+echo "Server running at http://127.0.0.1:8081\n";
+
+$loop->run();
